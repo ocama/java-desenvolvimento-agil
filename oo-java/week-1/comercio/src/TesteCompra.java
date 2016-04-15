47a8b3fc7eda52a0aaa93d637591da28be84ab20
OMAR ASSEM <ocama@hotmail.com>
2016-04-15 13:06:30 -0300

    2016-04-15-1306

---
+++ oo-java/week-1/comercio/src/TesteCompra.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteCompra {
+
+	@Test
+	public void compraAVista() {
+		Compra c = new Compra(500);
+		assertEquals(1, c.getNumeroParcelas());
+		assertEquals(500, c.getValorTotal());
+		assertEquals(500, c.GetValorParcela());
+	}
+
+	@Test
+	public void compraParcelada() {
+		Compra c = new Compra(4,250);
+		assertEquals(4, c.getNumeroParcelas());
+		assertEquals(1000, c.getValorTotal());
+		assertEquals(250, c.GetValorParcela());
+	}
+
+}
